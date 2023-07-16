ee640ab30d38749395d1c58ed2b9e4e7461099d0
yanyanyaa <yclin3411@gmail.com>
2023-07-16 23:02:16 +0800

    modify userSchema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String },
   email: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
