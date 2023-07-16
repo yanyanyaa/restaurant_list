2339307dfbd887686fe0c3096aff03277cb7e6d8
yanyanyaa <yclin3411@gmail.com>
2023-07-16 10:01:14 +0800

    add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -23,31 +23,47 @@ router.get('/register', (req, res) => {
 // 註冊
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符。' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '這個 email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
-        name,
-        email,
-        password
-      })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
     }
+    return User.create({
+      name,
+      email,
+      password
+    })
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   })
-    .catch(err => console.log(err))
 })
 
 // 登出
-router.get('/logout', (req,res) => {
+router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已成功登出')
   res.redirect('/users/login')
 })
 
